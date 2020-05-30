42aa53e54a0b2da85faade5f5e8e96f9cc9e70ff
maki1319 <wenchen1319@gmail.com>
2020-05-30 16:22:51 +0800

    spring boot例子

---
+++ src/main/java/com/sb/SpringBootApplicationDemo3.java
@@ -0,0 +1,12 @@
+package com.sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication//启动类
+public class SpringBootApplicationDemo3 {
+
+    public static void main(String[] args) {
+      SpringApplication.run(SpringBootApplicationDemo3.class,args);//必须要加的方法
+
+    }
+}
